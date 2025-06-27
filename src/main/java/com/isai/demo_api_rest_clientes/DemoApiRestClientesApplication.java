151402e6a158552f711fe063c9050e15f6598fa9
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-27 13:17:41 -0500

    init

---
+++ src/main/java/com/isai/demo_api_rest_clientes/DemoApiRestClientesApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_rest_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiRestClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiRestClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_api_rest_clientes/DemoApiRestClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_rest_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiRestClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
